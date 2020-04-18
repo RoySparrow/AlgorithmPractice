0c6d56b7f119350898e2f513bc2e671c78b85292
Roy Sparrow <rvxl2013@gmail.com>
2020-04-18 22:55:12 +0800

    [add] check integer array has duplicates func

--- AlgorithmPractice/AlgorithmPractice/Algorithm/Algorithm.swift
+++ AlgorithmPractice/AlgorithmPractice/Algorithm/Algorithm.swift
@@ -10,4 +10,22 @@ import Foundation
 
 class Algorithm {
     
+    /// 判斷在整數陣列中是否有重複的數值
+    func hasDuplicatesInIntegerArray(_ intArray: [Int]) -> Bool {
+        
+        // 需要它來達成取值 O(1)
+        var intValueDic: [Int: Int] = [:]
+        
+        for value in intArray {
+            
+            // 如果拿得到值，代表重複了
+            if intValueDic[value] != nil {
+                return true
+            }
+            
+            // 記錄，數值目前無意義
+            intValueDic[value] = 1
+        }
+        return false
+    }
 }
